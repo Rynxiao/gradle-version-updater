39be3717c63eaf433df8f18fe39126a1769a26f1
Rynxiao <yuzhongzi91@sina.com>
2022-10-21 18:50:07 +0800

    feat: add task list for version update

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,10 @@ import fsPromise from 'fs/promises';
 import fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
+import Listr from 'listr';
 
 const g2js = require('gradle-to-js/lib/parser');
+const execa = require('execa');
 const work_dir = process.cwd();
 const FILE_NAME = 'build.gradle';
 
@@ -79,7 +81,7 @@ const promptForMissingOptions = async (options) => {
   }
   const { currentVersion, patchVersion, minorVersion, majorVersion } = await getModuleVersions(selectedModule);
 
-  const versionAnswer = await inquirer.prompt([
+  const answers = await inquirer.prompt([
     {
       type: 'list',
       name: 'version',
@@ -87,16 +89,92 @@ const promptForMissingOptions = async (options) => {
       choices: [`patch (${patchVersion})`, `minor (${minorVersion})`, `major (${majorVersion})`],
       default: 'patch',
     },
+    {
+      type: 'confirm',
+      name: 'tagAndCommit',
+      message: 'Add tag and commit?',
+      default: true,
+    },
+    {
+      type: 'confirm',
+      name: 'shouldPush',
+      message: 'Push to remote?',
+      default: false,
+    },
   ]);
+
   return {
     ...options,
     module: selectedModule,
-    version: options.version || versionAnswer.version,
+    version: answers.version.match(/\d.\d.\d/g)[0],
+    tagAndCommit: answers.tagAndCommit,
+    shouldPush: answers.shouldPush,
   };
+};
+
+const writeVersion = async (options) => {
+  const filePath = path.join(work_dir, options.module, FILE_NAME);
+
+  try {
+    const fileContent = await fsPromise.readFile(filePath, 'utf-8');
+    const updatedFileContent = fileContent.replace(/version\s*"\d.\d.\d"/g, `version "${options.version}"`);
+    await fsPromise.writeFile(filePath, updatedFileContent, 'utf-8');
+  } catch (error) {
+    throw new Error('Update version failed');
+  }
+};
+
+const makeCommit = async (options, ctx) => {
+  const commitMessage = `release(N/A): upgrade ${options.module} to ${options.version}`;
+  const executeOptions = { cwd: work_dir };
+
+  try {
+    await execa('git', ['add', '.'], executeOptions);
+    await execa('git', ['commit', '-m', commitMessage], executeOptions);
+    await execa('git', ['tag', options.version], executeOptions);
+  } catch (error) {
+    ctx.isCommitFailed = false;
+    throw new Error('Tag and commit failed');
+  }
+};
+
+const pushCode = async () => {
+  const executeOptions = { cwd: work_dir };
+
+  try {
+    await execa('git', ['push', 'origin', '--tags'], executeOptions);
+  } catch (error) {
+    throw new Error('Push failed');
+  }
+};
+
+const runTasks = async (options) => {
+  const tasks = new Listr([
+    {
+      title: 'Update version',
+      task: () => writeVersion(options),
+    },
+    {
+      title: 'Commit and tag',
+      task: (ctx) => makeCommit(options, ctx),
+      skip: () => (options.tagAndCommit ? undefined : 'Skip commit'),
+    },
+    {
+      title: 'Commit and tag',
+      task: () => pushCode(options),
+      skip: (ctx) => {
+        const shouldPush = options.shouldPush && !ctx.isCommitFailed && options.tagAndCommit;
+        return shouldPush ? undefined : 'Skip push';
+      },
+    },
+  ]);
+
+  await tasks.run();
+  console.log(`\n🎉🎉🎉${chalk.green.bold('DONE')} Successfully!`);
 };
 
 export const cli = async (args) => {
   let options = parseArgumentsIntoOptions(args);
   options = await promptForMissingOptions(options);
-  console.log(options);
+  await runTasks(options);
 };
